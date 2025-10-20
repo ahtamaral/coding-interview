3f3c55bccbd0da306abdec77a60068344e681812
Artur Amaral <artur.amaral@mettainnovations.com.br>
2025-10-20 12:14:10 -0300

    Update 2

--- leetcode/2/run.py
+++ leetcode/2/run.py
@@ -1,3 +1,59 @@
+"""
+[9,9,9,9,9,9,9]
+[9,9,9,9]
+[8,9,9,9,]
+
+	[9] -> [9] -> [9] -> [9] -> [9] -> [9] -> [9] -> None
+
+	[9] -> [9] -> [9] -> [9] -> None
+	_______
+Carry    0      1     1      1      1      1      1      1
+Sum	18     18     18     18     9      9      9      0
+Value	[8] -> [9] -> [9] -> [9] -> [0] -> [0] -> [0] -> [1]
+
+
+Assumption: 
+It only stops, when we encounter a None on both list AND the carry for the next digit is zero. 
+
+Sum the two digits, defining the current node value and the carry for the next.
+
+Set the cur_node_value (digit_sum PLUS carry).
+Create a new node, appending it to the final of our list.
+
+If we encounter a None at list l1, for example, the contribution of that list to our digit_sum from now on will be zero. The same for l2. When both l1 and l2 next node are None, the execution ends. (Creating a final new node that will receive the sum contribution of l1 and l2, which is zero, plus the carry from the previous sum)
+
+carry_from_previous_node = 0
+
+
+while...
+
+	cur_node.val = l1_contribution + l2_contribution + carry_from_previous_node
+
+	if l1_cur_node.val == None:
+		l1_contribution = 0
+	else:
+		l1_contribution = l1_cur_node.val 
+
+	if l2_cur_node.val == None:
+		l2_contribution = 0
+	else:
+		l2_contribution = l2_cur_node.val
+		
+	digit_sum = l1_contribution + l2_contribution
+	
+	if digit_sum < 10:	
+		...
+		# carry_for_next_node = ...
+	
+	l3_cur_node.val = digit_sum + carry_from_previous_node
+	
+	carry_from_previous_node = carry_for_next_node
+	
+	# Advance one node on the list.
+	# At this point, one of them can be None. If this is the case, don't update anymore, just let the variable as none. When both become None, we break out of while loop.
+"""
+
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
